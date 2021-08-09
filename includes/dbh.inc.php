56a95907bf62b4e753427229ed8ccd300ecb9b96
ShiamMisbah <shiam.misbah42@gmail.com>
2021-08-10 01:48:26 +0600

    Update to dbh.inc.php

--- includes/dbh.inc.php
+++ includes/dbh.inc.php
@@ -2,9 +2,9 @@
 /* Database credentials. Assuming you are running MySQL
 server with default setting (user 'root' with no password) */
 $serverName = "localhost";
-$dbUsername = "thetatab_waitlistuser";
-$dbPassword = "?lU3AsMaMZ=E";
-$dbName = "thetatab_waitlist";
+$dbUsername = "root";
+$dbPassword = "";
+$dbName = "tap-network";
  
 /* Attempt to connect to MySQL database */
 $conn = mysqli_connect($serverName, $dbUsername, $dbPassword, $dbName);
